24c7ff6f33af21c76af7e8778c89349d05d66d64
Yulia Nanaeva <un681996@gmail.com>
2024-04-09 17:30:15 +0300

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+import 'swiper/swiper.scss'
+import 'swiper/modules/pagination.scss'
+import '../scss/style.scss'
+
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+
+// Кнопка "Показать все"
+var section = document.querySelector('main')
+var buttonShowAll = section.querySelector('.btn-show')
+
+buttonShowAll.addEventListener('click', function () {
+  section.classList.toggle('hide')
+  section.classList.toggle('show')
+
+  var buttonHide = document.querySelector('.hide')
+  var buttonShow = document.querySelector('.show')
+
+  if (buttonShow) {
+    buttonShowAll.textContent = 'Скрыть'
+  }
+
+  if (buttonHide) {
+    buttonShowAll.textContent = 'Показать все'
+  }
+})
+
+// Инициализация свайпера
+if (document.body.matchMedia < 768) {
+  swiper.init(swiper)
+}
+
+const swiper = new Swiper('.brends__swiper', {
+  spaceBetween: 16,
+  width: 240,
+
+  modules: [Pagination],
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewPort: true
+  },
+
+  on: {
+    resize: function enableOnlyMobile(swiper) {
+      if (768 < window.innerWidth) {
+        swiper.disable()
+        swiper.el.classList.add('-non-slider')
+      } else {
+        swiper.enable()
+        swiper.el.classList.remove('-non-slider')
+      }
+    }
+  }
+})
