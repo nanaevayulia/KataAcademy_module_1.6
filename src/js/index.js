704a5af02c87e76aac1823bee5112571836e8484
Yulia Nanaeva <un681996@gmail.com>
2024-04-11 17:53:45 +0300

    Popup

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import Swiper from 'swiper'
 import { Pagination } from 'swiper/modules'
 
 // Кнопка "Показать все"
-const section = document.querySelector('main')
+const section = document.querySelector('.content')
 const buttonShowAll = section.querySelector('.btn-show')
 const buttonText = buttonShowAll.textContent
 
@@ -26,7 +26,7 @@ buttonShowAll.addEventListener('click', function () {
   }
 })
 
-// Инициализация свайпера
+// Инициализация свайпера 1
 if (document.body.matchMedia < 768) {
   swiper.init(swiper)
 }
@@ -60,7 +60,7 @@ const swiper = new Swiper('.swiper', {
   }
 })
 
-// Инициализация свайпера
+// Инициализация свайпера 2
 if (document.body.matchMedia < 768) {
   priceSwiper.init(swiper)
 }
@@ -93,17 +93,104 @@ const priceSwiper = new Swiper('.price__swiper', {
   }
 })
 
-//Открыть меню
-const buttonMenu = document.querySelector('.icon--menu')
 const menu = document.querySelector('.burger-menu')
-const buttonClose = menu.querySelector('.icon--close')
+const content = document.querySelector('.content')
+const substrate = document.querySelector('.substrate')
+const popupCall = document.querySelector('.popup--call')
+const popupFeedback = document.querySelector('.popup--feedback')
+const buttonMenu = document.querySelector('.icon--menu')
+const buttonCloseMenu = menu.querySelector('.icon--close')
+const buttonPhoneMenu = menu.querySelector('.icon--phone--show')
+const buttonPhoneContent = document.querySelector('.icon--phone')
+const buttonFeedbackMenu = menu.querySelector('.icon--chat--show')
+const buttonFeedbackContent = document.querySelector('.icon--chat')
 
-buttonMenu.addEventListener('click', function (evt) {
-  evt.stopPropagation()
-
+// Открыть/закрыть меню
+buttonMenu.addEventListener('click', function () {
   menu.classList.add('show__burger-menu')
+  substrate.classList.add('show__substrate')
+  content.classList.add('stop-scrolling')
 })
 
-buttonClose.addEventListener('click', function () {
+buttonCloseMenu.addEventListener('click', function () {
   menu.classList.remove('show__burger-menu')
+  substrate.classList.remove('show__substrate')
+  content.classList.remove('stop-scrolling')
+  popupCall.classList.remove('show__popup--call')
+  popupFeedback.classList.remove('show__popup--feedback')
+})
+
+substrate.addEventListener('click', function () {
+  menu.classList.remove('show__burger-menu')
+  substrate.classList.remove('show__substrate')
+  content.classList.remove('stop-scrolling')
+  popupCall.classList.remove('show__popup--call')
+  popupFeedback.classList.remove('show__popup--feedback')
+})
+
+// Открыть/закрыть попап "Заказать звонок"
+function closePopup(popup, button) {
+  const buttonClose = popup.querySelector('.icon--close')
+  buttonClose.classList.add('close')
+
+  const close = document.querySelector('.close')
+
+  if (popup === popupCall && button === buttonPhoneMenu) {
+    close.addEventListener('click', function () {
+      popup.classList.remove('show__popup--call')
+      buttonClose.classList.remove('close')
+    })
+  } else if (popup === popupCall && button === buttonPhoneContent) {
+    close.addEventListener('click', function () {
+      popup.classList.remove('show__popup--call')
+      substrate.classList.remove('show__substrate')
+      content.classList.remove('stop-scrolling')
+      buttonClose.classList.remove('close')
+    })
+  } else if (popup === popupFeedback && button === buttonFeedbackMenu) {
+    close.addEventListener('click', function () {
+      popup.classList.remove('show__popup--feedback')
+      buttonClose.classList.remove('close')
+    })
+  } else if (popup === popupFeedback && button === buttonFeedbackContent) {
+    close.addEventListener('click', function () {
+      popup.classList.remove('show__popup--feedback')
+      substrate.classList.remove('show__substrate')
+      content.classList.remove('stop-scrolling')
+      buttonClose.classList.remove('close')
+    })
+  }
+}
+
+buttonPhoneMenu.addEventListener('click', function () {
+  popupCall.classList.toggle('show__popup--call')
+  substrate.classList.add('show__substrate')
+  popupFeedback.classList.remove('show__popup--feedback')
+
+  closePopup(popupCall, buttonPhoneMenu)
+})
+
+buttonPhoneContent.addEventListener('click', function () {
+  popupCall.classList.add('show__popup--call')
+  substrate.classList.add('show__substrate')
+  content.classList.add('stop-scrolling')
+
+  closePopup(popupCall, buttonPhoneContent)
+})
+
+// Открыть/закрыть попап "Обратная связь"
+buttonFeedbackMenu.addEventListener('click', function () {
+  popupFeedback.classList.toggle('show__popup--feedback')
+  substrate.classList.add('show__substrate')
+  popupCall.classList.remove('show__popup--call')
+
+  closePopup(popupFeedback, buttonFeedbackMenu)
+})
+
+buttonFeedbackContent.addEventListener('click', function () {
+  popupFeedback.classList.add('show__popup--feedback')
+  substrate.classList.add('show__substrate')
+  content.classList.add('stop-scrolling')
+
+  closePopup(popupFeedback, buttonFeedbackContent)
 })
